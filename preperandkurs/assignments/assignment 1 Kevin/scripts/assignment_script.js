831672d42135319ecca2747a0ec9b83949fd9a98
Ryngthm <ragefireinc@gmail.com>
2024-05-24 18:12:42 +0200

    Assignment 1 - Kevin

---
+++ preperandkurs/assignments/assignment 1 Kevin/scripts/assignment_script.js
@@ -0,0 +1,16 @@
+foo = 0
+bar = 0
+
+foo_number = "foo_number is = 0"
+bar_number = "bar_number is = 0"
+divided = foo / bar
+
+console.log(foo_number);
+console.log(bar_number);
+
+if (divided == NaN) {
+    alert("You can't divide us!")
+}
+function log() {
+    console.log('Divide');
+};
